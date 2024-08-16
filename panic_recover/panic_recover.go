ba6a06f183b7d477a3fee8118edfcabb5ea2bf61
guilherme-kunsch <guilherme.kunsch@outlook.com>
2024-08-15 23:53:13 -0300

    ajustando funcao recover

--- panic_recover/panic_recover.go
+++ panic_recover/panic_recover.go
@@ -3,8 +3,9 @@ package main
 import "fmt"
 
 func recuperarExecucao() {
-	fmt.Println("Tentativa de recuperar execução!")
-	recover()
+	if r := recover(); r != nil {
+		fmt.Println("Execução recuperada com sucesso!")
+	}
 }
 
 func mediaAluno(n1, n2 float32) bool {
