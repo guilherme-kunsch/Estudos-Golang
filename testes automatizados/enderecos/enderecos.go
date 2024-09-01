3ef269e9e8719d1e1f914ee8cd808a796024aa0b
guilherme-kunsch <guilherme.kunsch@outlook.com>
2024-09-01 19:23:47 -0300

    introdução a testes automatizados

---
+++ testes automatizados/enderecos/enderecos.go
@@ -0,0 +1,26 @@
+package enderecos
+
+import "strings"
+
+// tipo de endereço
+func TipoDeEndereco(endereco string) string {
+	tiposValidos := []string{"rua", "avenida", "estrada", "rodovia"}
+
+	enderecoLetraMinuscula := strings.ToLower((endereco))
+
+	primeiraPalavraEndereco := strings.Split(enderecoLetraMinuscula, " ")[0]
+
+	enderecoTemTipoValido := false
+
+	for _, tipo := range tiposValidos {
+		if tipo == primeiraPalavraEndereco {
+			enderecoTemTipoValido = true
+		}
+	}
+
+	if enderecoTemTipoValido {
+		return primeiraPalavraEndereco
+	}
+
+	return "Tipo Inválido"
+}
