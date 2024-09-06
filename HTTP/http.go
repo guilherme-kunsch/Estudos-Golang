873869c1781af23980420432de416576186d7e14
guilherme-kunsch <guilherme.kunsch@outlook.com>
2024-09-05 21:38:57 -0300

    http em go

---
+++ HTTP/http.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func home(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Olá mundo"))
+}
+
+func usuarios(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Usuário cadastrado"))
+}
+
+func main() {
+	http.HandleFunc("/home", home)
+	http.HandleFunc("/usuarios", usuarios)
+	log.Fatal(http.ListenAndServe(":5000", nil))
+}
